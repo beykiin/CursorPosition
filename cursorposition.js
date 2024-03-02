1cf7cfd743a8a067b89041498e506e31011e36f0
Yasin Beken <yasinbeken16@gmail.com>
2024-03-02 14:33:14 +0300

    First Commit

---
+++ cursorposition.js
@@ -0,0 +1,10 @@
+// window.onmousemove=(event)=>{
+//     let xpos=document.querySelector("#x")
+//     let ypos=document.querySelector("#y")
+//     xpos.innerHTML=event.clientX+"px"
+//     ypos.innerHTML=event.clientY+"px"
+// }
+
+function pencere(){
+    open("https://www.google.com.tr","_blank","width=200px,height=200px")
+}
